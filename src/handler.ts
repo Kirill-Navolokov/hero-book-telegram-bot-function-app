8ae5574dff84f50f90a2edacd53f3839939a20d4
Kyrylo <kirill.navolokov@gmail.com>
2025-08-28 02:51:14 +0300

    fix

--- src/handler.ts
+++ src/handler.ts
@@ -48,9 +48,9 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 
             await sendMessage({
                 chat_id: body.callback_query.message.chat.id,
-                //parse_mode: 'MarkdownV2',
+                parse_mode: 'MarkdownV2',
                 text: `(${wod.imageUrl})
-\\*${wod.name}*
+*${wod.name}*
 Дата виконання: ${wod.executionDate}
 
 Схема:
