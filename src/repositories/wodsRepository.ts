f959ed6ae1347b818c688844722df56e12d8d502
Kyrylo <kirill.navolokov@gmail.com>
2025-08-28 18:27:39 +0300

    clean up

--- src/repositories/wodsRepository.ts
+++ src/repositories/wodsRepository.ts
@@ -1,30 +1,17 @@
-import { inject, injectable } from "inversify";
-// import { TYPES } from "../ioc/typesMap";
-import { Db, MongoClient } from "mongodb";
+import { Db } from "mongodb";
 import { Wod } from "../models/wod";
-import { AppConfig } from "../appConfig";
 
-//@injectable()
 export class WodsRepository {
     private readonly db: Db;
 
-    constructor(
-        // @inject(TYPES.MongoClient) private readonly mongo: MongoClient,
-        // @inject(TYPES.AppConfig) private readonly config: AppConfig
-        mongo: Db,
-    ) {
+    constructor(mongo: Db) {
         this.db = mongo;
     }
 
     async getRandomWod(): Promise<Wod> {
         const collection = this.db.collection<Wod>(process.env.DB_WODS_COLLECTION!);
-
-        // Use $sample to get 1 random document
-        //const randomWod = await collection.aggregate<Wod>([{ $sample: { size: 1 } }]).toArray();
         const count = await collection.countDocuments();
-        // Step 2: generate random index
         const randomIndex = Math.floor(Math.random() * count);
-        // Step 3: fetch one document with skip
         const randomWod = await collection.find().skip(randomIndex).limit(1).toArray();
 
         return randomWod[0];
