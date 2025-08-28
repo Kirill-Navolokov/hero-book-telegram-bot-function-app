5682ebc580282c1086029c37bc5ca3ccfe41030b
Kyrylo <kirill.navolokov@gmail.com>
2025-08-28 03:42:22 +0300

    WIP

--- src/handler.ts
+++ src/handler.ts
@@ -50,7 +50,9 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
                 chat_id: body.callback_query.message.chat.id,
                 parse_mode: 'MarkdownV2',
                 //text: `(${wod.imageUrl})\n*${wod.name}*\nДата виконання: ${wod.executionDate}\n\nСхема:\n${wod.scheme}`
-                text: `*${wod.name}*\nСхема:\n${wod.scheme}`
+                text: `*${wod.name}*\n
+                Дата виконання: ${wod.executionDate.toLocaleDateString("uk-UA", {month:'long',day:'numeric'})}\n
+                Схема:\n${wod.scheme}`
             })
         } else {
             await sendMessage({
