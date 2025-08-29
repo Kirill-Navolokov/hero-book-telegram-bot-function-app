fc93e3282af32c530c712a735d09ca1cefd27671
Kyrylo <kirill.navolokov@gmail.com>
2025-08-29 04:13:45 +0300

    WIP

--- src/handler.ts
+++ src/handler.ts
@@ -68,30 +68,30 @@ async function handleCallbackQuery(callbackQuery: any): Promise<void> {
 async function handleReplyMessage(message: any): Promise<void> {
     const text = message.reply_to_message.text as string;
 
-    await sendMessage({
-        chat_id: message.chat.id,
-        text: JSON.stringify({
+    // await sendMessage({
+    //     chat_id: message.chat.id,
+    //     text: JSON.stringify({
+    //         chat_id: (process.env.HERO_BOOK_ADMIN_GROUP as unknown) as number,
+    //         text: strings.unitRegistrationRequest(message.text, message.from.username),
+    //         reply_markup: {
+    //             inline_keyboard: [[
+    //                 {text:'Затвердити'},
+    //                 {text:'Відхилити'}
+    //             ]]
+    //         }
+    //     })
+    // })
+    // if(text.startsWith(strings.unitRegistration)) {
+        await sendMessage({
             chat_id: (process.env.HERO_BOOK_ADMIN_GROUP as unknown) as number,
             text: strings.unitRegistrationRequest(message.text, message.from.username),
-            reply_markup: {
-                inline_keyboard: [[
-                    {text:'Затвердити'},
-                    {text:'Відхилити'}
-                ]]
-            }
-        })
-    })
-    // if(text.startsWith(strings.unitRegistration)) {
-        // await sendMessage({
-        //     chat_id: (process.env.HERO_BOOK_ADMIN_GROUP as unknown) as number,
-        //     text: strings.unitRegistrationRequest(message.text, message.from.username),
-        //     reply_markup: {
-        //         inline_keyboard: [[
-        //             {text:'Затвердити'},
-        //             {text:'Відхилити'}
-        //         ]]
-        //     }
-        // });
+            // reply_markup: {
+            //     inline_keyboard: [[
+            //         {text:'Затвердити'},
+            //         {text:'Відхилити'}
+            //     ]]
+            // }
+        });
     // } else if (text.startsWith(strings.businessRegistration)) {
 
     // } else {
