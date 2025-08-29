877df75e2a2cf4ca90a0b514e15cb1a62cc133ce
Kyrylo <kirill.navolokov@gmail.com>
2025-08-29 03:59:29 +0300

    FIX

--- src/handler.ts
+++ src/handler.ts
@@ -66,21 +66,22 @@ async function handleCallbackQuery(callbackQuery: any): Promise<void> {
 
 async function handleReplyMessage(message: any): Promise<void> {
     const text = message.reply_to_message.text as string;
-    if(text.startsWith(strings.unitRegistration)) {
+    // if(text.startsWith(strings.unitRegistration)) {
         await sendMessage({
             chat_id: (process.env.HERO_BOOK_ADMIN_GROUP as unknown) as number,
             text: strings.unitRegistrationRequest(message.text, message.from.username),
             reply_markup: {
-                inline_keyboard: [
-                    [{text:'Затвердити'}, {text:'Відхилити'}]
-                ]
+                inline_keyboard: [[
+                    {text:'Затвердити'},
+                    {text:'Відхилити'}
+                ]]
             }
         });
-    } else if (text.startsWith(strings.businessRegistration)) {
+    // } else if (text.startsWith(strings.businessRegistration)) {
 
-    } else {
+    // } else {
 
-    }
+    // }
 }
 
 const knowsUsers = new Set<string>();
