7e601f7f142570c312cd158b920d885fadec9c1d
Kyrylo <kirill.navolokov@gmail.com>
2025-09-01 22:09:30 +0300

    code refactored

--- src/handler.ts
+++ src/handler.ts
@@ -50,13 +50,30 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 async function validateRequestUser(body: any): Promise<boolean> {
     let user: User | undefined = undefined;
     let chatId = body.message?.chat?.id ?? body.callback_query.message.chat.id;
+    let message = '';
 
     if(body.message) {
         user = body.message.from as User;
+        message = body.message.text;
     }
-    //  else if(body.callback_query) {
-    //     user = body.callback_query.from as User;
-    // }
+     else if(body.callback_query) {
+        user = body.callback_query.from as User;
+        message = body.callback_query.data;
+    }
+
+    if(user) {
+        const tgAccountRepo = new TelegramAccountsRepository(mongoClient.db(process.env.DB_NAME));
+        if(await tgAccountRepo.isBanned(user.id)) {
+            await sendMessage({
+                chat_id: chatId,
+                text: strings.userBanned
+            });
+            return false;
+        }
+    }
+
+    if(message == botCommands.start || message == botCommands.randomWod)
+        return true;
 
     if(!user) {
         await sendMessage({
@@ -78,15 +95,6 @@ async function validateRequestUser(body: any): Promise<boolean> {
         await sendMessage({
             chat_id: chatId,
             text: strings.usernameNotDefined
-        });
-        return false;
-    }
-
-    const tgAccountRepo = new TelegramAccountsRepository(mongoClient.db(process.env.DB_NAME));
-    if(await tgAccountRepo.isBanned(user.id)) {
-        await sendMessage({
-            chat_id: chatId,
-            text: strings.userBanned
         });
         return false;
     }
