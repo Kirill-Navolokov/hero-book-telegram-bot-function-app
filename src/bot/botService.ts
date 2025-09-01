08f314d6d5b11bff3ea8f545eeeb289d989c5e33
Kyrylo <kirill.navolokov@gmail.com>
2025-09-01 23:17:13 +0300

    FIX

--- src/bot/botService.ts
+++ src/bot/botService.ts
@@ -2,9 +2,70 @@ import fetch from "node-fetch";
 import BotPhotoResponse from "../models/botPhotoResponse";
 import BotTextResponse from "../models/botTextResponse";
 import BotResponse from "../models/botResponse";
+import { User } from "../models/user";
+import { TelegramAccountsRepository } from "../repositories/telegramAccountsRepository";
+import { mongoClient } from "../handler";
+import { strings } from "./strings";
+import { botCommands } from "./commands";
 
 const TOKEN = process.env.TELEGRAM_TOKEN!;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+
+export async function validateRequestUser(body: any): Promise<boolean> {
+    let user: User | undefined = undefined;
+    let chatId = body.message?.chat?.id ?? body.callback_query.message.chat.id;
+    let message = '';
+
+    if(body.message) {
+        user = body.message.from as User;
+        message = body.message.text;
+    } else if(body.callback_query) {
+        user = body.callback_query.from as User;
+        message = body.callback_query.data;
+    }
+
+    if(user) {
+        const tgAccountRepo = new TelegramAccountsRepository(mongoClient.db(process.env.DB_NAME));
+        if(await tgAccountRepo.isBanned(user.id)) {
+            await sendMessage({
+                chat_id: chatId,
+                text: strings.userBanned
+            });
+            return false;
+        }
+    }
+
+    if(message == botCommands.start || 
+        message == botCommands.randomWod || 
+        message == botCommands.needMoreFunctionality)
+        return true;
+
+    if(!user) {
+        await sendMessage({
+            chat_id: chatId,
+            text: strings.cantIdentifyUser
+        });
+        return false;
+    }
+
+    if(user.is_bot) {
+        await sendMessage({
+            chat_id: chatId,
+            text: strings.botsNotAllowed
+        });
+        return false;
+    }
+
+    if(user.username == undefined) {
+        await sendMessage({
+            chat_id: chatId,
+            text: strings.usernameNotDefined
+        });
+        return false;
+    }
+
+    return true;
+}
 
 export function sendMessage(responseMessage: BotTextResponse): Promise<fetch.Response> {
     return sendToBot('sendMessage', responseMessage);
