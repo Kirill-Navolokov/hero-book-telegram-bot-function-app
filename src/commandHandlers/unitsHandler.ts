d4534e0d332729cc8c3201c94ac7b636b354865f
Kyrylo <kirill.navolokov@gmail.com>
2025-09-13 01:55:55 +0300

    WIP

--- src/commandHandlers/unitsHandler.ts
+++ src/commandHandlers/unitsHandler.ts
@@ -216,6 +216,7 @@ export async function handleUnitUpdateInput(
             break;
         case botCommands.setPhoto:
             const photos = (message.photo as Array<any>);
+            await sendMessage({chat_id: chatId, text: JSON.stringify(message)});
             if(photos.length == 0 || photos.length > 1)
                 await sendMessage({chat_id: chatId, text: 'МОЖНА ЗАВАНТАЖИТИ ТІЛЬКИ ОДНЕ ФОТО ДЛЯ ЛОГО'});
             else
