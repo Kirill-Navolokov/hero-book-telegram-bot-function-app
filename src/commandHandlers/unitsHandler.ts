50c4fecfdbcbdd8fa830fec29ae6f8d9c5b82e01
Kyrylo <kirill.navolokov@gmail.com>
2025-09-13 01:51:37 +0300

    photo upload POC

--- src/commandHandlers/unitsHandler.ts
+++ src/commandHandlers/unitsHandler.ts
@@ -215,6 +215,11 @@ export async function handleUnitUpdateInput(
                 await sendMessage({chat_id: chatId, text: strings.foundationDateValidation});
             break;
         case botCommands.setPhoto:
+            const photos = (message.photo as Array<any>);
+            if(photos.length == 0 || photos.length > 1)
+                await sendMessage({chat_id: chatId, text: 'МОЖНА ЗАВАНТАЖИТИ ТІЛЬКИ ОДНЕ ФОТО ДЛЯ ЛОГО'});
+            else
+                await sendMessage({chat_id: chatId, text: 'ФОТО Є'});
             break;
         default:
             await sendMessage({chat_id: chatId, text: strings.unknownRequest(action)})
