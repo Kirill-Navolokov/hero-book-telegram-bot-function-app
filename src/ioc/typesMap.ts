48bac8821b1307e7fd5e733d005997c953f8c917
Kyrylo <kirill.navolokov@gmail.com>
2025-09-18 01:45:28 +0300

    WIP

--- src/ioc/typesMap.ts
+++
@@ -1,6 +0,0 @@
-// export const TYPES = {
-//     AppConfig: Symbol.for('AppConfig'),
-//     WodsRepo: Symbol.for('WodsRepo'),
-//     MongoClient: Symbol.for('MongoClient'),
-//     DbClient: Symbol.for('DbClient')
-// };
