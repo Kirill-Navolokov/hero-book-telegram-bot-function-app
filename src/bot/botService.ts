d8e1d6e7a7c2f411be1ce63ca3ee104aa84e7206
Kyrylo <kirill.navolokov@gmail.com>
2025-09-17 16:35:27 +0300

    fix

--- src/bot/botService.ts
+++ src/bot/botService.ts
@@ -141,10 +141,14 @@ export function deleteMessage(deleteMessage: BotDeleteMessageResponse): Promise<
 }
 
 export function getFile(fileInfoRequest: FileInfoRequest): Promise<fetch.Response> {
-    return sendToBot('file', fileInfoRequest);
+    return fetch(`https://api.telegram.org/file/bot${TOKEN}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(fileInfoRequest)
+    });
 }
 
-function sendToBot(endpoint: string, response: any): Promise<fetch.Response> {
+function sendToBot(endpoint: string, response: BotResponse): Promise<fetch.Response> {
     return fetch(`${TELEGRAM_API}/${endpoint}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
